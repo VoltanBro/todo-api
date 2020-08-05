9ac5eed8b56c090af5c915bc1c2a1bc620a3120c
Vitaly Molchanov <etc.sharam@gmail.com>
2020-08-05 22:13:46 +0300

    Add test for User model

--- spec/api/v1/models/user_spec.rb
+++ spec/api/v1/models/user_spec.rb
@@ -7,6 +7,8 @@ RSpec.describe User, type: :model do
     @user = create(:user)
     @user2 = build(:user, name: 'Joe', password: '123_@asd',
                           password_confirmation: '123_@asd')
+    @user3 = build(:user, name: 'Ivan', password: 'abcd1234',
+                          password_confirmation: 'abcd1234')
   end
 
   it 'is valid with valid attributes' do
@@ -14,5 +16,9 @@ RSpec.describe User, type: :model do
   end
   it 'is invalid with invalid attributes' do
     expect(@user2).to be_invalid
+  end
+  it 'is invalid with a duplicate user name' do
+    @user3.valid?
+    expect(@user3.errors[:name]).to include('has already been taken')
   end
 end
