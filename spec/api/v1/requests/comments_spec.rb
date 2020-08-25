9959eae21846ffb8ca6c6193fc44ad06d1d49bf9
Vitaly Molchanov <etc.sharam@gmail.com>
2020-08-25 22:49:19 +0300

    Add .idea and coverage folders to gitignore

--- spec/api/v1/requests/comments_spec.rb
+++ spec/api/v1/requests/comments_spec.rb
@@ -1,14 +1,14 @@
 require 'swagger_helper'
 
 RSpec.describe 'api/projects', type: :request do
-  let(:user) { create(:user) }
-  let(:project) { create(:project, user_id: user.id) }
-  let(:task) { create(:task, user_id: user.id, project_id: project.id) }
-  let(:comment) { create(:comment, user_id: user.id, project_id: project.id, task_id: task.id) }
-  let(:payload) { {user_id: user.id} }
-  let(:session) { JWTSessions::Session.new(payload: payload) }
-  let(:tokens) { session.login }
-  let(:params) { {comment: {content: 'test_789_102312', project_id: project.id, task_id: task.id } } }
+  let(:user)          { create(:user) }
+  let(:project)       { create(:project, user_id: user.id) }
+  let(:task)          { create(:task, user_id: user.id, project_id: project.id) }
+  let(:comment)       { create(:comment, user_id: user.id, project_id: project.id, task_id: task.id) }
+  let(:payload)       { { user_id: user.id } }
+  let(:session)       { JWTSessions::Session.new(payload: payload) }
+  let(:tokens)        { session.login }
+  let(:params)        { {comment: {content: 'test_789_102312', project_id: project.id, task_id: task.id } } }
   let(:Authorization) { "Bearer #{tokens[:access]}" }
 
   path '/api/v1/comments' do
